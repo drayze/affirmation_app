164f5acdfe4b78e5c31acfb6f86f0eb1d90a7962
drayze54 <drayze54@gmail.com>
2025-11-12 05:03:56 +0000

    Fully connecting to firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,10 +3,11 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.drayze.b_kind_2_u"
+    namespace = "com.drayze.be_kind_2_u"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +22,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.drayze.b_kind_2_u"
+        applicationId = "com.drayze.be_kind_2_u"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = 1
@@ -46,4 +47,6 @@ flutter {
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
     implementation("androidx.activity:activity-ktx:1.8.0")
+    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
